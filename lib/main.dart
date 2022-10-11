b4843a92904bdd5d0ddb6e0fd9c041c2db1ea845
nasir4440 <abdulghanihusein@gmail.com>
2022-10-11 09:35:52 +0300

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,204 +1,203 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const CardExamplesApp());
-}
+void main()=>runApp(MyApp());
 
-class CardExamplesApp extends StatelessWidget {
-  const CardExamplesApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            //Pizzamenu(),
-            pizza1(),
-            pizza2(),
-            pizza3(),
-            pizza4(),
-          ],
-        ),
+        // appBar: AppBar(
+        //   leading: Icon(Icons.menu),
+        //   title: Center(child: Text('PIZZA DELIVERY APP',)),
+        //   actions: [
+        //     Padding(
+        //       padding: const EdgeInsets.all(20.0),
+        //       child: Icon(Icons.search),
+        //     )
+        //   ],
+        // ),
+        backgroundColor: Colors.cyan[50],
+        body: PizzaCard(),
       ),
     );
   }
 }
 
-
-
-
-class pizza1 extends StatelessWidget {
-  const pizza1({Key? key}) : super(key: key);
+class PizzaCard extends StatelessWidget {
+  const PizzaCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Card(
-        elevation: 20,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        child: Row(
-          children: [
-            Container(
+      child: Column(
+        children:[
+          Container(
+            child: Card(
 
-              child:
-              Image.asset('images/pizza2.jpg'),height: 100,width: 200,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
+              elevation: 5,
+              child: Center(
+                child: Image.asset('images/pizza4.jpg'),
+              ),
+            ),height: 200,
+            width: 350,
+          ),
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50),),
+            elevation: 15,
+            child: Row(
               children: [
-                Text('Pizza'),
-                Text('Available'),
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(3.0),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage ('images/pizza2.jpg',),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15,),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text('Pizza Somali',style: TextStyle(fontFamily: 'BreeSerif'),),
+                Text('Available',style: TextStyle(fontFamily: 'RobotoMono'),),
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
+                    Icon(Icons.star,size: 15,color: Colors.green,),
+                    Icon(Icons.star,size: 15,color: Colors.green,),
+                    Icon(Icons.star,size: 15,color: Colors.green,),
+                    Icon(Icons.star,size: 15,color: Colors.green,),
+                    Icon(Icons.star,size: 15,color: Colors.green,),
                   ],
                 )
               ],
+              ),
+                SizedBox(width: 70,),
+                Text('\$100',style: TextStyle(fontFamily: 'BreeSerif')),
+              ],
             ),
-            SizedBox(width: 30),
-            Text('\$11')
+          ),
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50),),
+            elevation: 15,
+            child: Row(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(3.0),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage ('images/pizza4.jpg',),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15,),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Pizza UAE',style: TextStyle(fontFamily: 'BreeSerif'),),
+                    Text('Available',style: TextStyle(fontFamily: 'RobotoMono'),),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(width: 80,),
+                Text('\$80',style: TextStyle(fontFamily: 'BreeSerif')),
+              ],
+            ),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50),),
+            elevation: 15,
+            child: Row(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(3.0),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage ('images/pizza1.jpg',),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15,),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Pizza Kenyan',style: TextStyle(fontFamily: 'BreeSerif'),),
+                    Text('Available',style: TextStyle(fontFamily: 'RobotoMono'),),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(width: 70,),
+                Text('\$55',style: TextStyle(fontFamily: 'BreeSerif'),),
+              ],
+            ),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50),),
+            elevation: 15,
+            child: Row(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(3.0),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage ('images/pizza3.jpg',),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15,),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Pizza KSA',style: TextStyle(fontFamily: 'BreeSerif'),),
+                    Text('Available',style: TextStyle(fontFamily: 'RobotoMono'),),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                        Icon(Icons.star,size: 15,color: Colors.green,),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(width: 80,),
+                Text('\$40',style: TextStyle(fontFamily: 'BreeSerif')),
+              ],
+            ),
+          ),
 
 
-          ],
-        ),
+        ]
       ),
     );
   }
 }
 
-class pizza2 extends StatelessWidget {
-  const pizza2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Card(
-        elevation: 20,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        child: Row(
-          children: [
-            Container(
-
-              child:
-              Image.asset('images/pizza3.jpg'),height: 100,width: 200,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Pizza'),
-                Text('Available'),
-                Row(
-                  children: [
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                  ],
-                )
-              ],
-            ),
-            SizedBox(width: 30),
-            Text('\$11')
-
-
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class pizza3 extends StatelessWidget {
-  const pizza3({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Card(
-        elevation: 20,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        child: Row(
-          children: [
-            Container(
-
-              child:
-              Image.asset('images/pizza3.jpg'),height: 100,width: 200,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Pizza'),
-                Text('Available'),
-                Row(
-                  children: [
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                  ],
-                )
-              ],
-            ),
-            SizedBox(width: 30),
-            Text('\$11')
-            
-
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class pizza4 extends StatelessWidget {
-  const pizza4({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Card(
-        elevation: 20,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        child: Row(
-          children: [
-            Container(
-
-              child:
-              Image.asset('images/pizza3.jpg'),height: 100,width: 200,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Pizza'),
-                Text('Available'),
-                Row(
-                  children: [
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                    Icon(Icons.star,size: 18,color: Colors.green,),
-                  ],
-                )
-              ],
-            ),
-            SizedBox(width: 30),
-            Text('\$11')
-
-
-          ],
-        ),
-      ),
-    );
-  }
-}
 
